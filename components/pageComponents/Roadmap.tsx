416775346712d94ba2295b1ed6717724e5c242ba
Lakmi <lakmi94@gmail.com>
2023-06-08 11:29:41 +0900

    roadmap desktop completed

--- components/pageComponents/Roadmap.tsx
+++ components/pageComponents/Roadmap.tsx
@@ -2,8 +2,11 @@
 
 import { Flex, Link, Text, useTheme } from "@chakra-ui/react";
 import RoadmapCard from "../roadmap/RoadmapCard";
+import Image from "next/image";
 import Comb from "../roadmap/Comb";
 import useMediaView from "@/app/hooks/useMediaView";
+import roadmapLine from "../../app/assets/roadmapLine.png";
+import TitanLogo from "../../app/assets/Titan_symbol.svg";
 function Roadmap() {
   const theme = useTheme();
   const { mobileView, tabletView, pcView } = useMediaView();
@@ -33,24 +36,51 @@ function Roadmap() {
         based on market conditions and other factors.{" "}
       </Text>
 
-      <Flex flexDir={"column"}  border={'1px solid red'} pl='168px'>
-        <Flex h="233px" alignItems={"flex-end"} mb='25px' position={'relative'} top='75px'>
-          <RoadmapCard top={""} left="" />
-          <RoadmapCard top={"-83px"} left={"-40px"} />
-          <RoadmapCard top={""} left={"-80px"} />
-          <RoadmapCard top={"-83px"} left={"-120px"} />
-          <RoadmapCard top={""} left={"-160px"} />
+      <Flex
+        flexDir={"column"}
+        pl="168px"
+        backgroundImage={roadmapLine.src}
+        backgroundRepeat={'no-repeat'}
+        backgroundPosition={'center'}
+        backgroundSize='985px 5px'
+      >
+        <Flex
+          h="233px"
+          alignItems={"flex-end"}
+          mb="25px"
+          position={"relative"}
+          top="75px"
+        >
+          <RoadmapCard top={""} left="" completed={true} title='L2 Mainnet Open' subtitle="" />
+          <RoadmapCard top={"-83px"} left={"-40px"} completed={true} title='Unified Bridge & Swap Open' subtitle=""/>
+          <RoadmapCard top={""} left={"-80px"} completed={false} title='Native Fast Withdrawal Open' subtitle=""/>
+          <RoadmapCard top={"-83px"} left={"-120px"} completed={false} title='TONStarter (Launchpad) L2 Service Open' subtitle=""/>
+          <RoadmapCard top={""} left={"-160px"} completed={false} title='On-Demand Service Open' subtitle=""/>
         </Flex>
-        <Flex justifyContent={'space-between'}>
-        <RoadmapCard top={"0px"} left={"180px"} />
-        
+        <Flex justifyContent={"space-between"}>
+          <Flex
+            top={"0px"}
+            position={"relative"}
+            left={"235px"}
+            w="222px" /* Adjust the width as per your requirement */
+            h="152px"
+          >
+            <Image height={100} width={115} src={TitanLogo} alt="TitanLogo" />
+          </Flex>
+          {/* <RoadmapCard top={"0px"} left={"180px"} completed={false} /> */}
         </Flex>
-       <Flex alignItems={"flex-start"} h="233px" mt='25px' position={'relative'} top='-75px'>
-          <RoadmapCard top={""} left="" />
-          <RoadmapCard top={"83px"} left={"-40px"} />
-          <RoadmapCard top={""} left={"-80px"} />
-          <RoadmapCard top={"83px"} left={"-120px"} />
-          <RoadmapCard top={""} left={"-160px"} />
+        <Flex
+          alignItems={"flex-start"}
+          h="233px"
+          mt="25px"
+          position={"relative"}
+          top="-75px"
+        >
+          <RoadmapCard top={""} left="" completed={true}  title='DARIUS L2 (Goerli) Testnet Open' subtitle="TESTNET"/>
+          <RoadmapCard top={"83px"} left={"-40px"} completed={false} title='Optimism Bedrock Integration' subtitle="" />
+          <RoadmapCard top={""} left={"-80px"} completed={false} title='Holešky (Holešovice) Testnet Open' subtitle=""/>
+          <RoadmapCard top={"83px"} left={"-120px"} completed={false} title='Improved Fee Structure Integration' subtitle=""/>
+          <RoadmapCard top={""} left={"-160px"} completed={false} title='Optimism OP Stack Maintenance' subtitle="CONTINOUS"/>
         </Flex>
       </Flex>
 
--- components/roadmap/RoadmapCard.tsx
+++ components/roadmap/RoadmapCard.tsx
@@ -1,33 +1,69 @@
-import { Flex, Text, Button, Box } from "@chakra-ui/react";
+import { Flex, Text, Button, Box, useTheme } from "@chakra-ui/react";
 import Image from "next/image";
-import tokamak_symbol from '../../app/assets/tokamak_symbol.png'
+import Check from "../../app/assets/icon-check.svg";
 
 type RoadmapProps = {
   top: string;
-  left:string
-}
-function RoadmapCard(props:RoadmapProps ) {
-  const {top, left} = props
+  left: string;
+  completed: boolean;
+  title: string;
+  subtitle: string;
+};
+function RoadmapCard(props: RoadmapProps) {
+  const { top, left, completed, title, subtitle } = props;
+  const theme = useTheme();
   return (
     <Box
-    top={top}
-    left={left}
-      w="220px" /* Adjust the width as per your requirement */
-      h="150px" /* Adjust the height as per your requirement */
-     backgroundImage={'linear-gradient(to bottom, #1f8efa, #2a72e5)'}
-     display={'inline-block'}
-     justifyContent={'center'}
-     alignItems={'center'}
-      position='relative'
+      top={top}
+      left={left}
+      w="222px" /* Adjust the width as per your requirement */
+      h="152px" /* Adjust the height as per your requirement */
+      bg={completed ? "none" : "#dfe4ee"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      position="relative"
       overflow="hidden"
       clipPath="polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)"
-      
     >
-        
-        <Text color={'black'}>hiiiid</Text>
-
+      <Box
+        top={"1px"}
+        left={"1px"}
+        w="220px" /* Adjust the width as per your requirement */
+        h="150px" /* Adjust the height as per your requirement */
+        backgroundImage={
+          completed ? "linear-gradient(to bottom, #1f8efa, #2a72e5)" : ""
+        }
+        bg={completed ? "" : "white"}
+        display={"flex"}
+        justifyContent={"space-evenly"}
+        alignItems={"center"}
+        position="absolute"
+        overflow="hidden"
+        zIndex={1}
+        flexDir={'column'}
+        clipPath="polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)"
+      >
+        <Flex>
+          <Image src={Check} alt=""/>
+        </Flex>
+        <Text
+          fontFamily={theme.fonts.openSans}
+          fontSize={"16px"}
+          fontWeight={600}
+          color={completed ? "#ffffff" : "#464d59"}
+          zIndex={10000}
+          // position={"absolute"}
+          textAlign={"center"}
+          px={title === 'L2 Mainnet Open'?'65px': "35px"}
+        >
+          {title}
+        </Text>
+        <Text mt='25px'  fontSize={"12px"}
+          fontWeight={600}
+          color={completed ? "#ffff07" : "#46bb75"}>{subtitle}</Text>
+      </Box>
     </Box>
-   
   );
 }
 
