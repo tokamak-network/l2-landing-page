290434ebf8c9427f3f73516c9d6147dcdf162ba2
Jananadi Wedagedara <jananadiw@gmail.com>
2023-06-23 15:30:53 +0900

    fix: Graphics - star styles

--- components/animations/Optimism/OptimismAnime.tsx
+++ components/animations/Optimism/OptimismAnime.tsx
@@ -4,6 +4,8 @@ import "./styles.css";
 export default function OptimismAnime() {
   return (
     <Box className="container">
+      <Box className="container four-point-star" />
+      <Box className="container four-point-star-small" />
       <Box className="container dotted-circle" />
       <Box className="container circle" />
       <Box className="container top-left-quarter-circle" />
